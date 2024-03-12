d18500795bf25b360c4d479b6b8b07be320a1b92
Tatiana Loskutova <tanya.loskutova333@gmail.com>
2024-03-12 21:25:35 +0300

    first check

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'tanishaloskut.zapto.org', '127.0.0.1', '62.84.120.12']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -92,15 +92,16 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated', 
+        'rest_framework.permissions.IsAuthenticated',
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
