f67388c9ec27e48ef8f6e9e8c22b7ab71ea94e66
Tatiana Loskutova <tanya.loskutova333@gmail.com>
2024-03-15 18:48:22 +0300

    ch

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -4,13 +4,14 @@ from pathlib import Path
 from django.core.management.utils import get_random_secret_key
 from dotenv import load_dotenv
 
-load_dotenv()
-
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env_path = BASE_DIR / '.env'
+load_dotenv(dotenv_path=env_path)
 
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 DEBUG = bool(os.getenv('DEBUG', 'false').lower() == 'true')
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost', '127.0.0.1').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
